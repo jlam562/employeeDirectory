a80b686bad7b0a16e1d9a65f8dfce38f7ea0bb92
jlam <jennylamwork@gmail.com>
2020-04-06 23:37:36 -0700

    added navbar

---
+++ src/components/Form/Form.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Form = () => {
+  return(
+    <div class="uk-margin">
+      <input class="uk-input uk-form-width-medium uk-form-small" type="text" placeholder="Employee Search">
+    </div>
+      <button class="uk-button uk-button-default">Submit</button>
+  )
+}
+
+export default Form
+
+
+
+
+
---
+++ src/components/Navbar/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar () => {
+  return(
+    <div class="uk-navbar-container" uk-navbar>
+      <div class="uk-navbar-left">
+        <h1 class="uk-heading-divider">Employee Directory</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
