a1ec1f772a45c4374f18cd01b96849561a0fe4a9
jlam <jennylamwork@gmail.com>
2020-04-07 02:04:19 -0700

    not finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Form from './components/Form'
+import Card from './components/Card'
+
+class App extends Component {
+  state = {
+    input: '',
+    search: '',
+    category: 'company_name',
+    selected: ''
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  
+}
 
 function App() {
   return (
     <Navbar />
     <Form />
+    <Card />
   );
 }
 
---
+++ src/components/Card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card() => {
+  return (
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+      <h3 class="uk-card-title">Default</h3>
+      <p>Lorem ipsum <a href="#">dolor</a> sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  )
+}
+
+export default Card
